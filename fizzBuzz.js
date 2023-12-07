a1abd01d8b2fa33ecc5d8a69986f669bc3656f01
rhidtech <abdulsalamridwan16@gmail.com>
2023-12-07 13:10:44 +0100

    bmi calculator

---
+++ fizzBuzz.js
@@ -0,0 +1,23 @@
+var output = [];
+var count = 1;
+function fizzBuzz() {
+    while (count <= 100) {
+         if(count % 3 === 0 && count % 5 === 0){
+   output.push("fizzBuzz");
+}
+else if (count % 3 === 0) {
+    output.push("fizz");
+}
+else if(count % 5 === 0){
+   output.push("Buzz");
+}
+       else{
+  output.push(count);
+}
+count++;
+    console.log(output);
+}
+    }
+while (true) {
+
+}
